b5e43ae72d8c31e6a4078501833b2e87d2c48ba5
Christopher_McIntosh <ChristopherM@tlgcohort.com>
2024-07-18 13:26:03 -0500

    Added test

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,26 +9,51 @@
 package com.math;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class CalculatorTest {
+    //business objects under test - called the "fixture"
+    private Calculator calc;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        System.out.println("After Class");
+    }
+
+    @Before
+    public void setUp(){
+        System.out.println("setUp");
+        calc = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+
+    }
 
     @Test
     public void testisEven() {
-        Calculator calc = new Calculator();
+        System.out.println("testEven");
         assertTrue(calc.isEven(10));
         assertFalse(calc.isEven(11));
     }
 
     @Test
     public void testDivide() {
-        Calculator calc = new Calculator();
+        System.out.println("testDivide");
         assertEquals(2.5,calc.divide(5, 2), .001);
     }
 
     @Test
     public void testAdd() {
-        Calculator calc = new Calculator();
+        System.out.println("testAdd");
         assertEquals(5, calc.add(1, 4));  // expected, actual
     }
 }
