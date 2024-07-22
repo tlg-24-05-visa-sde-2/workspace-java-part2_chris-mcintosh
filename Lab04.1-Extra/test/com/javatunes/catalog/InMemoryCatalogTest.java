15951aecb1fe1245800c0ed4586afea9705a4665
Christopher_McIntosh <ChristopherM@tlgcohort.com>
2024-07-22 14:19:32 -0500

    InMemoryCatalog<MusicItem> - writing unit Tests

--- Lab04.1-Extra/test/com/javatunes/catalog/InMemoryCatalogTest.java
+++ Lab04.1-Extra/test/com/javatunes/catalog/InMemoryCatalogTest.java
@@ -1,6 +1,10 @@
 package com.javatunes.catalog;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
 public class InMemoryCatalogTest {
@@ -10,5 +14,32 @@ public class InMemoryCatalogTest {
     @Before
     public void setUp() {
         catalog = new InMemoryCatalog();
+    }
+
+    @Test
+    public void findByCategory_shouldReturnCollectionWithThatCategory() {
+        Collection<MusicItem> popItems = catalog.findByCategory(MusicCategory.POP);
+
+        assertEquals(4, popItems.size());
+
+        for (MusicItem item : popItems) {
+            assertEquals(MusicCategory.POP, item.getMusicCategory());
+        }
+    }
+
+    @Test
+    public void findBy_Id_shouldReturnMusicItemWithThatId_idFound() {
+        MusicItem item = catalog.findById(18L);
+
+        assertNotNull(item);
+        assertEquals(18L, item.getId().longValue());
+
+
+    }
+
+    @Test
+    public void findById_shouldReturnNull_idNotFound() {
+        MusicItem item = catalog.findById(1001L);
+        assertNull(item);
     }
 }
