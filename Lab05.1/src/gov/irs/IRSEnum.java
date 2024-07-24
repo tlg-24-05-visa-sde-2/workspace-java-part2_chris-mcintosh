713110d3c753afd9f39bf750416cef62971319d3
Christopher_McIntosh <ChristopherM@tlgcohort.com>
2024-07-24 11:46:20 -0500

    updated tax client

---
+++ Lab05.1/src/gov/irs/IRSEnum.java
@@ -0,0 +1,30 @@
+/*
+ * This code is sample code, provided as-is, and we make no
+ * warranties as to its correctness or suitability for any purpose.
+ *
+ * We hope that it's useful to you.  Enjoy.
+ * Copyright LearningPatterns Inc.
+ */
+
+package gov.irs;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public enum IRSEnum implements IRS {
+    INSTANCE;
+
+    // BUSINESS CODE
+    private static Collection<TaxPayer> payers = new ArrayList<>();
+
+    public void collectTaxes() {
+        for (TaxPayer payer : payers) {
+            payer.payTaxes();
+        }
+    }
+
+    public void register(TaxPayer payer) {
+        payers.add(payer);
+        // no-op
+    }
+}
