687978c914e2373d5a80e8c093b709f86fc231cf
Christopher_McIntosh <ChristopherM@tlgcohort.com>
2024-07-26 14:38:33 -0500

    Comparator of String as Lambda

--- Lab08.1/test/com/javatunes/compare/StringSortTest.java
+++ Lab08.1/test/com/javatunes/compare/StringSortTest.java
@@ -14,7 +14,8 @@ import org.junit.Test;
 
 public class StringSortTest {
 
-    private List<String> names = Arrays.asList("Jason", "Martin", "Jack", "Mary", "Jill", "Frederick", "Laurie", "Ed");
+    private final List<String> names = Arrays.asList(
+            "Jason", "Martin", "Jack", "Mary", "Jill", "Frederick", "Laurie", "Ed");
 
     @Test
     public void testStringListSort() {
@@ -31,6 +32,8 @@ public class StringSortTest {
 
         System.out.println("Increasing length - lambda:");
         // TODO: call names.sort() and pass in an "expression" lambda
+        names.sort((name1, name2) -> name1.length() - name2.length());
+        System.out.println(names + "\n");
         // hint: a compatible lambda will have this form:
         //       (obj1, obj2) -> expression-that-evaluates-to-int
     }
